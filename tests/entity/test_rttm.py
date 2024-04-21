668882be570fb0be12f8eec35835e2f701ce6116
kittchy <drums.radwimps203@gmail.com>
2024-04-21 18:55:43 +0900

    [ADD] generate_test

--- tests/entity/test_rttm.py
+++ tests/entity/test_rttm.py
@@ -11,11 +11,11 @@ def test_rttm_object():
         channel_id=1,
         turn_onset=0.0,
         turn_duration=1.0,
-        orthography_field="<NA>",
-        speaker_type="<NA>",
         speaker_name="hogehoge",
-        confidence_score="<NA>",
-        signal_lookahead_time="<NA>",
+        orthography_field=None,
+        speaker_type=None,
+        confidence_score=None,
+        signal_lookahead_time=None,
     )
     assert rttm
     assert rttm.type == "SPEAKER"
@@ -23,8 +23,8 @@ def test_rttm_object():
     assert rttm.channel_id == 1
     assert rttm.turn_onset == 0.0
     assert rttm.turn_duration == 1.0
-    assert rttm.orthography_field == "<NA>"
-    assert rttm.speaker_type == "<NA>"
+    assert rttm.orthography_field is None
+    assert rttm.speaker_type is None
     assert rttm.speaker_name == "hogehoge"
-    assert rttm.confidence_score == "<NA>"
-    assert rttm.signal_lookahead_time == "<NA>"
+    assert rttm.confidence_score is None
+    assert rttm.signal_lookahead_time is None
