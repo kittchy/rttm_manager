98e2eb57c91fc1b7e6f7f4e9f0890b5398944ff1
kittchy <drums.radwimps203@gmail.com>
2024-04-21 18:55:59 +0900

    [ADD] importer exporter

--- src/rttm_manager/__init__.py
+++ src/rttm_manager/__init__.py
@@ -1,6 +1,11 @@
-def import_rttm() -> str:
-    return "Imported rttm!"
+from rttm_manager.entity.rttm import RTTM
+from rttm_manager.service.importer import RTTMImporter
+from rttm_manager.service.exporter import RTTMExporter
 
 
-def export_rttm() -> str:
-    return "Exported rttm!"
+def import_rttm(file_path: str) -> list[RTTM]:
+    return RTTMImporter.rttm_import(file_path)
+
+
+def export_rttm(rttms: list[RTTM], file_path: str) -> None:
+    RTTMExporter.rttm_export(rttms, file_path)
